7272335fc7e33bb4830b7bed20ce3595b0cb01ef
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-02-21 20:02:57 -0800

    chat page added with openai llm

--- utils/action.js
+++ utils/action.js
@@ -1,6 +1,24 @@
 'use server'
 
-export const generateChatResponse = async (chatMessage) => {
-    console.log(chatMessage)
-    return 'hello friend'
+import OpenAI from "openai"
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+export const generateChatResponse = async (chatMessages) => {
+    try {
+        const response = await openai.chat.completions.create({
+            messages: [
+                {role: 'system', content: 'you are a helpful travel planning assistant name Galvin'},
+                ...chatMessages
+            ],
+            model: 'gpt-3.5-turbo',
+            temperature: 0.1
+        })
+    
+        return response.choices[0].message
+    } catch (error) {
+        return null
+    }
 }
