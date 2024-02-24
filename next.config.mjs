1accab0ae8558fae56721aa110e9db88772082ed
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-02-24 13:37:12 -0800

    single tour page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+        port: '',
+        pathname: '/private/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- utils/action.js
+++ utils/action.js
@@ -113,3 +113,25 @@ export const getExistingTour = async ({ city, country }) => {
     });
     return tours;
   };
+
+  export const getSingleTour = async (id) => {
+    return prisma.tour.findUnique({
+      where: {
+        id,
+      },
+    });
+  };
+
+
+  export const generateTourImage = async ({ city, country }) => {
+    try {
+      const tourImage = await openai.images.generate({
+        prompt: `a panoramic view of the ${city} ${country}`,
+        n: 1,
+        size: '512x512',
+      });
+      return tourImage?.data[0]?.url;
+    } catch (error) {
+      return null;
+    }
+  };
